30160dee2827d28c106c31af240ad5fa5d802186
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-08-27 16:01:38 +0200

    Add Generic Host and dependency injection

--- Telenec.Mail.App/App.xaml.cs
+++ Telenec.Mail.App/App.xaml.cs
@@ -1,14 +1,43 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
-namespace Telenec.Mail.App
+namespace Telenec.Mail.App;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly IHost _host;
+
+    public App()
     {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.StartAsync()
+            .GetAwaiter()
+            .GetResult();
+
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _host.StopAsync()
+            .GetAwaiter()
+            .GetResult();
+
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
 }
