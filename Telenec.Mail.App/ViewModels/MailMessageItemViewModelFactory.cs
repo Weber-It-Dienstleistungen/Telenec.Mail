d2211b4f2ae6684293dd6c92aeebed4420633d9e
Weber-It-Dienstleistungen <fabian.weber@weber-it-nec.de>
2026-09-23 10:46:46 +0200

    Add mail message view model factory

---
+++ Telenec.Mail.App/ViewModels/MailMessageItemViewModelFactory.cs
@@ -0,0 +1,98 @@
+﻿using Telenec.Mail.App.Models;
+
+namespace Telenec.Mail.App.ViewModels;
+
+public static class MailMessageItemViewModelFactory
+{
+    public static MailMessageItemViewModel Create(
+        MailMessageData message)
+    {
+        ArgumentNullException.ThrowIfNull(
+            message);
+
+        return new MailMessageItemViewModel(
+            sender:
+                message.Sender,
+
+            senderAddress:
+                message.SenderAddress,
+
+            recipientAddress:
+                message.RecipientAddress,
+
+            subject:
+                message.Subject,
+
+            preview:
+                message.Preview,
+
+            displayTime:
+                message.DisplayTime,
+
+            displayDateTime:
+                message.DisplayDateTime,
+
+            senderInitial:
+                message.SenderInitial,
+
+            greeting:
+                message.Greeting,
+
+            body:
+                message.Body,
+
+            closing:
+                message.Closing,
+
+            signature:
+                message.Signature,
+
+            isUnread:
+                message.IsUnread,
+
+            emphasizeSender:
+                message.EmphasizeSender,
+
+            highlightTitle:
+                message.HighlightTitle,
+
+            highlightText:
+                message.HighlightText,
+
+            htmlBody:
+                message.HtmlBody,
+
+            uniqueId:
+                message.UniqueId,
+
+            attachments:
+                message.Attachments,
+
+            hasSmimeSignature:
+                message.HasSmimeSignature,
+
+            messageId:
+                message.MessageId,
+
+            references:
+                message.References,
+
+            toAddresses:
+                message.ToAddresses,
+
+            ccAddresses:
+                message.CcAddresses,
+
+            replyToAddresses:
+                message.ReplyToAddresses,
+
+            importance:
+                message.Importance,
+
+            readReceipt:
+                message.ReadReceipt,
+
+            receivedReadReceipts:
+                message.ReceivedReadReceipts);
+    }
+}
